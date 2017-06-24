22a308abe4aee128befd43f41cab9bc0645c7051
Nalipp <nateplipp@gmail.com>
2017-06-24 17:16:54 +0900

    second commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Learn Teach Code Attendance Tracker
+## Track Attendance
+
+## Award Badges
+
+## Display Leaderboard
+
+## Basic live messaging
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Learn Teach Code Attendance Tracker' });
 });
 
 module.exports = router;
