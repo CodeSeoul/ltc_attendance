ee40a32886909763157c09016e74e6b233942818
Nalipp <nateplipp@gmail.com>
2017-07-11 16:52:21 +0900

    course model validations

--- models/Course.js
+++ models/Course.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const Course = mongoose.model('Course', {
-  title: {type: String, required: true},
-  description: String,
+  title: {
+    type: String, 
+    required: [true, 'Title is required'],
+    validate: {
+      validator: (title) => title.length > 2 && title.length < 100,
+      message: 'Title must be valid length'
+    }
+  },
+  description: {
+    type: String, 
+    validate: {
+      validator: (description) => 
+      description.length > 2 && description.length < 10000,
+      message: 'Description must be valid length'
+    }
+  },
   tags: [String],
-  createdAt: Date,
+  createdAt: { type: Date, default: Date.now },
   createdBy: String
 })
 
