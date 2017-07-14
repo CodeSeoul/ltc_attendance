55eeaf4bfbd94d914231c2097446aa03f3df8df7
Nalipp <nateplipp@gmail.com>
2017-07-14 13:07:21 +0900

    fix travis error

--- test/user/repo.test.js
+++ test/user/repo.test.js
@@ -61,7 +61,7 @@ describe('User Repo routes', () => {
 
   it('should update existing user with updateUser()', (done) => {
     joe.email = 'm@m.com'; 
-    Repo.updateUser(joe._id, joe, result => {
+    Repo.updateUser({_id: joe._id}, joe, result => {
       Repo.getUser(joe._id, result => {
         result.email.should.be.equal('m@m.com');
         done();
