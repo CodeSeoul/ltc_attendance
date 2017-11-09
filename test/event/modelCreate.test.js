8753069fecbd41fbdb1f9eccfb34a968d92f50d6
Bryan "Beege" Berry <draco4king@gmail.com>
2017-11-09 13:58:18 +0900

    Trying to configure bookshelf for testing, but the helper isn't running

--- test/event/modelCreate.test.js
+++ test/event/modelCreate.test.js
@@ -1,62 +1,61 @@
 const Event = require('../../models/Event');
 const User = require('../../models/User');
-const config = require('../config.test.js');
 const assert = require('assert');
-const mongoose = require('mongoose');
 
 describe('Event modelCreate', () => {
-  let sql;
 
-  beforeEach((done) => {
-    sql = new Event({title: 'sql'})
-    sql.save()
-      .then(() => done());
-  });
-  afterEach((done) => {
-    User.collection.drop();
-    Event.collection.drop();
-    done();
-  });
+    let sql;
 
-  it('Should create a new Event record', (done) => {
-    assert(!sql.isNew);
-    done();
-  });
-
-  it('Should be able to set Event Description', (done) => {
-    sql.description = "beginner sql";
-    sql.save()
-      .then((result) => {
-        assert(result.description === "beginner sql");
-        done();
-      })
-  });
-
-  it('Should be able to set Event Tags', (done) => {
-    sql.tags = ['sql', 'beginner']
-    sql.save()
-      .then(() => Event.findOne({title: 'sql'}))
-      .then((result) => {
-        assert(result.tags[0] === 'sql');
-        assert(result.tags[1] === 'beginner');
-        done();
-      });
-  });
-
-  it('Should set CreatedAt timestamp by default', (done) => {
-    assert(sql.createdAt instanceof Date);
-    done();
-  });
-
-  it('Should be able to set CreatedBy', (done) => {
-    const joe = new User({});
-    sql.instructors.push({_id: joe._id})
-    sql.save()
-      .then(() => Event.findOne({title: 'sql'}))
-      .then((result) => {
-        assert(String(result.instructors) === String(joe._id));
+    beforeEach((done) => {
+        sql = new Event({title: 'sql'})
+        sql.save()
+            .then(() => done());
+    });
+    afterEach((done) => {
+        User.collection.drop();
+        Event.collection.drop();
         done();
     });
-  });
+
+    it('Should create a new Event record', (done) => {
+        assert(!sql.isNew);
+        done();
+    });
+
+    it('Should be able to set Event Description', (done) => {
+        sql.description = "beginner sql";
+        sql.save()
+            .then((result) => {
+                assert(result.description === "beginner sql");
+                done();
+            })
+    });
+
+    it('Should be able to set Event Tags', (done) => {
+        sql.tags = ['sql', 'beginner']
+        sql.save()
+            .then(() => Event.findOne({title: 'sql'}))
+            .then((result) => {
+                assert(result.tags[0] === 'sql');
+                assert(result.tags[1] === 'beginner');
+                done();
+            });
+    });
+
+    it('Should set CreatedAt timestamp by default', (done) => {
+        assert(sql.createdAt instanceof Date);
+        done();
+    });
+
+    it('Should be able to set CreatedBy', (done) => {
+        const joe = new User({});
+        sql.instructors.push({_id: joe._id})
+        sql.save()
+            .then(() => Event.findOne({title: 'sql'}))
+            .then((result) => {
+                assert(String(result.instructors) === String(joe._id));
+                done();
+            });
+    });
 
 });
--- test/test_helper.test.js
+++ test/test_helper.test.js
@@ -1,4 +1,21 @@
 const bookshelfSetup = require('../config/bookshelf');
 
+console.log('before');
 bookshelfSetup.configureBookshelf('dev');
 bookshelfSetup.initializeDevDb();
+
+const CheckIns = require('../models/checkIn').CheckIns;
+const Users = require('../models/User').Users;
+const Events = require('../models/Event').Events;
+
+beforeEach(done => {
+    console.log('beforeEach');
+    CheckIns.destroy()
+        .then(() => {
+            return Users.destroy();
+        })
+        .then(() => {
+            Events.destroy();
+            done();
+        })
+});
