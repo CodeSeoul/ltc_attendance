6ac1210eafc8d70944d63f65fa517732f045f3c4
Hazelton Jacob <jhazelton11@gmail.com>
2017-11-11 17:49:29 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp')
+var cleancss = require('gulp-clean-css')
+
+gulp.task('css', function() {
+  return gulp.src("public/stylesheets/style.css")
+    .pipe(cleancss())
+    .pipe(gulp.dest('public/dist/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('public/stylesheets/style.css', ['css'])
+});
